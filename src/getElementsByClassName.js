7a695eb206fcdf628c741b4fde000074dfcb04ae
Ajay Tungare <ajay.tungare@gmail.com>
2014-04-29 19:13:35 -0400

    draft implementation of getElementsByClassName()

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,26 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
-	return document.getElementsByClassName(className);
+	return recursiveGEBCN(className, document.body);
+};
+
+var recursiveGEBCN = function(className, node) {
+	var result = [];
+	var myClassList = node.classList;
+	var myChildNodes = node.childNodes;
+
+	for (elem in myClassList) {
+		if (elem.indexOf(className) > -1) {
+			result.push(elem);
+		}
+	}
+
+	for (child in myChildNodes) {
+		var childRes = recursiveGEBCN(className, child);
+		for (item in childRes) {
+			result.push(item);
+		}
+	}
+
+	return result;
 };
